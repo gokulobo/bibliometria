d2e679a4506d1ad4f5fcf4d8c503a281b1c20ebd
eli <lobo13256@gmail.com>
2015-08-09 20:30:46 -0430

    cambio en el js cohorte

--- js/cohorte.js
+++ js/cohorte.js
@@ -1,6 +1,7 @@
 $(function () {
     $('select').material_select();
 });
+
 function guardar(){
     var formulario = $("form").serialize();
     $.ajax({url : "guardarCohorte.php",type : "POST",data:formulario,
