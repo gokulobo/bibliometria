0b17478cc4a6175db7ef399c1e5d99dd726ec715
gokulobo <white.1.wolf@hotmail.com>
2015-09-02 01:56:07 -0430

    cambios

--- sistema.sql
+++ sistema.sql
@@ -10,6 +10,11 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
+-- Volcando estructura de base de datos para sistema
+CREATE DATABASE IF NOT EXISTS `sistema` /*!40100 DEFAULT CHARACTER SET latin1 COLLATE latin1_spanish_ci */;
+USE `sistema`;
+
+
 -- Volcando estructura para tabla sistema.alumnos
 CREATE TABLE IF NOT EXISTS `alumnos` (
   `id_alumno` int(18) NOT NULL AUTO_INCREMENT,
